18e8f87a554dfd768b029502da97117aceeb7cd2
Aekoi <85933338+Aekoi@users.noreply.github.com>
2023-05-12 23:22:04 +0300

    added employee manager, changed cli to client

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from ui import Ui
 from client_manager import ClientManager
 from appointment_manager import AppointmentManager
+from employee_manager import EmployeeManager
 from utils import *
 from datetime import datetime
 
 DATABASE_FILE = "app.db"
 
-cli_manager = ClientManager(DATABASE_FILE)
+client_manager = ClientManager(DATABASE_FILE)
 appointment_manager = AppointmentManager(DATABASE_FILE)
+employee_manager = EmployeeManager(DATABASE_FILE)
 
-app = Ui(cli_manager)
+app = Ui(client_manager)
+
+# TEST ACCOUNT ('dr.georgepapadopoulos@gmail.com', 'sphgkfocygdxfneu')
+
